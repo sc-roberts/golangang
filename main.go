3792df6a552620339cc04f464ec3993086e47c76
Robert Berzins <robert.berzins@safetyculture.io>
2023-11-24 11:02:19 +1100

    comment added

--- main.go
+++ main.go
@@ -12,11 +12,14 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/hello", helloHandler)
 
-	port := ":8069"
+	port := ":8081"
 
 	fmt.Printf("Server listening on http://localhost%s\n", port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Printf("Error: %s\n", err)
 	}
+
 }
+
+// Graceful exit needed that shuts down the listener/kills the process
